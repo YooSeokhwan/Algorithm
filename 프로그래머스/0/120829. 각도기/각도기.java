e2fc48f96e74a1a4e2e83e3c5690cfc760109d7a
YooSeokHwan <ysh50227@gmail.com>
2024-08-12 12:24:55 +0900

    [level 0] Title: 각도기, Time: 0.03 ms, Memory: 75.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/120829. 각도기/각도기.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int solution(int angle) {
+        int answer = 0;
+        int a=1;
+        int b=2;
+        int c=3;
+        int d=4;
+        if (0< angle && angle < 90) {
+            answer = a;
+        } else if  (angle ==90) {
+            answer = b;
+        } else if (90<angle && angle <180) {
+            answer = c;
+        } else {
+            answer = d;
+        }
+        return answer;
+    }
+}
