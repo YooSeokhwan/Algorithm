9e2a69cbf64d16880da674b6d54d8a2021fcb1fe
YooSeokHwan <ysh50227@gmail.com>
2024-08-12 14:30:33 +0900

    [level 0] Title: 짝수의 합, Time: 0.05 ms, Memory: 77.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/120831. 짝수의 합/짝수의 합.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int n) {
+        int sum=0;
+        for(int i =0; i<n + 1; i++) {
+            if(i%2 == 0) {
+                sum += i;
+            }
+        }
+            return sum;
+
+    }
+}
