df24aa4f54bae244b5c46286dee006fbe7383d68
YooSeokHwan <ysh50227@gmail.com>
2024-08-28 17:49:33 +0900

    [level 2] Title: 소수 찾기, Time: 6.79 ms, Memory: 88.8 MB -BaekjoonHub

---
+++ 프로그래머스/2/42839. 소수 찾기/소수 찾기.java
@@ -0,0 +1,37 @@
+import java.util.*;
+
+class Solution {
+    Set<Integer> candidates = new HashSet<>();
+    public int solution(String numbers) {
+        permutation (numbers, 0, new boolean[numbers.length()] , 0);
+        
+        int answer = 0;
+        for(int num : candidates) {
+            if(isPrime(num)) answer++;
+        }
+        return answer;
+    }
+    void permutation(String numbers, int current, boolean[] visited, int digit) {
+        if(digit == numbers.length()) return;
+        
+        for(int i=0; i<numbers.length(); i++) {
+            if(visited[i]) continue;
+            
+            int newValue = current + (int)((numbers.charAt(i) - '0') * Math.pow(10, digit));
+            candidates.add(newValue);
+            
+            visited[i] = true;
+            permutation(numbers, newValue, visited, digit +1);
+            visited[i] = false;
+        }
+    }
+    
+    boolean isPrime(int n) {
+        if (n<2) return false;
+        
+        for (int i =2; i*i<=n; i++) {
+            if(n%i==0) return false;
+        }
+        return true;
+    }
+}
